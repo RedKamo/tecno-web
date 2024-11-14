098e4717e4e7098c4f63a1c5f3f0a368c1890adf
casuarez <camiloandressuarez1@gmail.com>
2024-11-13 20:10:52 -0500

    tecnotranspiler page, UI fix bento and hero front page. add animated diagram

---
+++ src/data/products.js
@@ -0,0 +1,39 @@
+export const products = [
+{
+    icon: '/icons/logoTtranspiler.svg',
+    title: 'TecnoTranspiler',
+    description: 'Herramienta para migrar sistemas Cliente/Servidor a entornos web modernos, facilitando la transición a arquitecturas multicapas.',
+    link: '/tecnotranspiler',
+  },
+  {
+    icon: '/icons/logoBancaria.svg',
+    title: 'Solución bancaria',
+    description: 'Solución para el sector financiero, optimiza la administración de captaciones, colocaciones y caja.',
+    link: '/solucion-bancaria',
+  },
+  {
+    icon: '/icons/logoTesoreria.svg',
+    title: 'Tesorería financiera',
+    description: 'Solución dirigida a empresas financieras, que facilita la integración con fuentes de datos de mercado como Bloomberg, Reuters y SetFx, entre otras.',
+    link: '/servicios/solucion-bancaria',
+  },
+  {
+    icon: '/icons/logoFiduciaria.svg',
+    title: 'Solución fiduciaria',
+    description: 'Solución para fiduciarias, diseñada para gestionar eficientemente patrimonios autónomos, fiducias y fondos de inversión.',
+    link: '/servicios/tecnotranspiler',
+  },
+  {
+    icon: '/icons/logoBolsa.svg',
+    title: 'Comisionistas de Bolsa',
+    description: 'Solución especializada para comisionistas de bolsa, diseñada para gestionar eficientemente las inversiones de terceros y optimizar la administración de portafolios.',
+    link: '/servicios/tecnotranspiler',
+  },
+  {
+    icon: '/icons/logoFabrica.svg',
+    title: 'Fábrica de Software',
+    description: 'Servicio de desarrollo de software para el sector financiero, creando soluciones personalizadas para nuevas tecnologías, negocios y servicios.',
+    link: '/servicios/tecnotranspiler',
+  },
+
+];
