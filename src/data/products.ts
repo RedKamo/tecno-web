f1bf4ea68a438419d82b259f4611369837ab71e6
casuarez <camiloandressuarez1@gmail.com>
2024-11-20 13:33:55 -0500

    Add PDF files, tecno-selva page, add prizes and certifications section, delete icons folder

--- src/data/products.ts
+++ src/data/products.ts
@@ -7,37 +7,37 @@ export interface Product {
 
 export const products: Product[] = [
   {
-    icon: '/icons/logoTtranspiler.svg',
+    icon: 'tabler:hexagon-letter-t',
     title: 'TecnoTranspiler',
     description: 'Herramienta para migrar sistemas Cliente/Servidor a entornos web modernos, facilitando la transición a arquitecturas multicapas.',
     link: '/tecnotranspiler'
   },
   {
-    icon: '/icons/logoBancaria.svg',
+    icon: 'tabler:building-bank',
     title: 'Solución Bancaria',
     description: 'Solución para el sector financiero, optimiza la administración de captaciones, colocaciones y caja.',
     link: '/solucion-bancaria'
   },
   {
-    icon: '/icons/logoTesoreria.svg',
+    icon: 'tabler:cash-banknote',
     title: 'Tesorería Financiera',
     description: 'Solución dirigida a empresas financieras, que facilita la integración con fuentes de datos de mercado como Bloomberg, Reuters y SetFx, entre otras.',
     link: '/tesoreria-financiera'
   },
   {
-    icon: '/icons/logoFiduciaria.svg',
+    icon: 'tabler:briefcase',
     title: 'Solución Fiduciaria',
     description: 'Solución para fiduciarias, diseñada para gestionar eficientemente patrimonios autónomos, fiducias y fondos de inversión.',
     link: '/solucion-fiduciaria'
   },
   {
-    icon: '/icons/logoBolsa.svg',
+    icon: 'tabler:chart-candle',
     title: 'Comisionistas de Bolsa',
     description: 'Solución especializada para comisionistas de bolsa, diseñada para gestionar eficientemente las inversiones de terceros y optimizar la administración de portafolios.',
     link: '/comisionistas-de-bolsa'
   },
   {
-    icon: '/icons/logoFabrica.svg',
+    icon: 'tabler:code',
     title: 'Fábrica de Software',
     description: 'Servicio de desarrollo de software para el sector financiero, creando soluciones personalizadas para nuevas tecnologías, negocios y servicios.',
     link: '/fabrica-software'
