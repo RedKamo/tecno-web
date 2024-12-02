845fc4c634ba759bc6d2c44cbffaaa2a719a6d14
casuarez <camiloandressuarez1@gmail.com>
2024-12-02 12:45:26 -0500

    feature- add tawkto widget, persiste all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import swup from '@swup/astro';
 
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), 
+  integrations: [tailwind(),
     icon({
       include: {
         tabler: ['*'],
       },
-    })]
+    }),
+    swup(),]
 });
